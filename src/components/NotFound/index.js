beec0461be8c6b0644490d92d9c4abf930bfe541
Hemanth Jyothula <hemanthjyothula1@gmail.com>
2023-12-23 01:23:36 +0000

    Hello

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,12 @@
+import {Component} from 'react'
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1>not found component</h1>
+      </div>
+    )
+  }
+}
+export default NotFound
